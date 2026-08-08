f40748a91d1c41290fb54667f7b1ced72636489a
Vimal Kansotia <71843101+vimal-kansotia@users.noreply.github.com>
2026-08-09 02:38:02 +0530

    Update import paths for page modules

--- app.py
+++ app.py
@@ -236,36 +236,36 @@ def main():
     # Route to selected page
     page_module = page_options[selected_page]
     
-    if page_module == "overview":
-        from overview import show
+   if page_module == "overview":
+        from pages.overview import show
         show(st.session_state.df)
     
     elif page_module == "analytics":
-        from analytics import show
+        from pages.analytics import show
         show(st.session_state.df)
     
     elif page_module == "clinical":
-        from clinical import show
+        from pages.clinical import show
         show(st.session_state.df)
     
     elif page_module == "ml_lab":
-        from ml_lab import show
+        from pages.ml_lab import show
         show(st.session_state.df, st.session_state.model)
     
     elif page_module == "predictions":
-        from predictions import show
+        from pages.predictions import show
         show(st.session_state.df, st.session_state.model)
     
     elif page_module == "explainability":
-        from explainability import show
+        from pages.explainability import show
         show(st.session_state.df, st.session_state.model)
     
     elif page_module == "explorer":
-        from explorer import show
+        from pages.explorer import show
         show(st.session_state.df)
     
     elif page_module == "research":
-        from research import show
+        from pages.research import show
         show(st.session_state.df, st.session_state.model)
 
 if __name__ == "__main__":
